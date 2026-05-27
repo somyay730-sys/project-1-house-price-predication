33fe84f1211ca6aff8128c1079f943e37399621e
Somya Y <somyay730@gmail.com>
2026-05-27 13:41:55 +0530

    Delete gggg.py

--- gggg.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-d=np.array([["red"],["blue"],["green"],["red"]])
-e=OneHotEncoder(sparse_output=True)
-e=e.fit_transform(d)
-print(e)
